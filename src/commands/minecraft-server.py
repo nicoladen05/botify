ebe9f55288157d584ac3ee673a4c61d6f5f05c56
nicoladen05 <nicolashartmanntaba@gmail.com>
2024-04-20 13:27:21 +0200

    :sparkles: Minecraft Server Status Update

---
+++ src/commands/minecraft-server.py
@@ -0,0 +1,55 @@
+from discord.ext import commands, tasks
+from mcstatus import JavaServer
+
+STATUS_CHANNEL = 1231197975313911848
+
+# Make sure to include the port
+SERVER_IP = "nerduniverse.duckdns.org:25565"
+
+
+def get_server_stats():
+    server = JavaServer.lookup(SERVER_IP)
+
+    try:
+        status = server.status()
+    except ConnectionRefusedError:
+        return False, 0
+
+    return True, status.players.online
+
+
+class Misc(commands.Cog):
+    def __init__(self, bot):
+        print("[COG] Minecraft Cog loaded!")
+        self.bot = bot
+        self.channel = self.bot.get_channel(STATUS_CHANNEL)
+        self.players = None
+        self.online = None
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.channel = self.bot.get_channel(STATUS_CHANNEL)
+        self.update_channel.start()
+
+    @tasks.loop(seconds=60)
+    async def update_channel(self):
+        online, players = get_server_stats()
+
+        # Return if no changes
+        if online == self.online and players == self.players:
+            return
+
+        # Run this if stats changed
+        self.online = online
+        self.players = players
+
+        if online:
+            channel_name = f"⛏️ 🟢 Online | 👤 {players}"
+        else:
+            channel_name = "⛏️ 🔴 Offline"
+
+        await self.channel.edit(name=channel_name)
+
+
+def setup(bot):
+    bot.add_cog(Misc(bot))
